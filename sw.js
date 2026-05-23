446e523f9eaa30cf9a4ca1300642f470ebdf39ae
Pancetaman28 <davidfdez90@gmail.com>
2026-05-23 08:48:49 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='nuestra-app-v19';
+const CACHE='nuestra-app-v20';
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{
